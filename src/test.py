b9860c2c1a4db729d2114dc3c5ab2b7a17a03843
wutong666888 <917992367@qq.com>
2024-07-04 19:55:46 +0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -15,6 +15,12 @@ chrome_options = Options()
 # chrome_options.add_argument('--disable-dev-shm-usage')
 # chrome_options.add_argument('--ignore-certificate-errors')
 # chrome_options.add_argument('--ignore-ssl-errors')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
 chrome_options.add_experimental_option("detach",True)
 
 ch = [
