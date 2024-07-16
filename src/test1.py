8ff1fedefe61df9920dbd240bd786aa9863ecf84
wutong666888 <917992367@qq.com>
2024-07-16 15:26:28 +0800

    Update test1.py

--- src/test1.py
+++ src/test1.py
@@ -15,6 +15,7 @@ chrome_options.add_argument("--disable-web-security")
 chrome_options.add_argument("--use-fake-ui-for-media-stream")
 chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
 chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+# chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 driver = webdriver.Chrome(options = chrome_options)
 
 # 使用WebDriver访问网页
